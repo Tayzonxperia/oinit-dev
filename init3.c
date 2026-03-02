bc8adc271751e8f58047b46094e5e116d00f0cad
okla <87949054+okthela@users.noreply.github.com>
2026-03-02 16:31:29 +0000

    Update init3.c

--- init3.c
+++ init3.c
@@ -11,14 +11,12 @@ extern char **environ;
 
 int main() {
     pid_t pid;
-    printf("STAGE 3: TTY and SUPERVISION.\n");
-    printf("oINIT v.1C\nExactly 100 lines of code!\n");
     posix_spawn(&pid, "/sbin/agetty", 0, 0, (char*[]){"/sbin/agetty", "-L", "tty1", "--noclear", "115200", "vt100", NULL}, environ);
     posix_spawn(&pid, "/sbin/agetty", 0, 0, (char*[]){"/sbin/agetty", "-L", "tty2", "--noclear", "115200", "vt100", NULL}, environ);
     posix_spawn(&pid, "/sbin/agetty", 0, 0, (char*[]){"/sbin/agetty", "-L", "tty3", "--noclear", "115200", "vt100", NULL}, environ);
     posix_spawn(&pid, "/sbin/agetty", 0, 0, (char*[]){"/sbin/agetty", "-L", "tty4", "--noclear", "115200", "vt100", NULL}, environ);
     printf("\nBoot complete!\n-----------------------------------------------------------------\n");
     printf("oINIT v.1C\n");
-    printf("Exactly 100 lines of code! :3\n");
+    printf("No idea how many lines of code this has!\n");
     while (1){sleep(1);}
 }
